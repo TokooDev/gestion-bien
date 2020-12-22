88ee19eb330c3f5eded9928e2e672174535d7e8b
@Tokodev <layesall05@gmail.com>
2020-12-22 09:59:04 +0000

    Ajout de pipes

--- src/app/biens/list-biens/item-bien/item-bien.component.ts
+++ src/app/biens/list-biens/item-bien/item-bien.component.ts
@@ -13,4 +13,14 @@ export class ItemBienComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  numberSeparator(nbre: number, sep: string){
+    let nombre: string = nbre.toString();
+    var reg = /(\d+)(\d{3})/;
+    while( reg.test( nombre)) {
+      nombre = nombre.replace( reg, '$1' +sep +'$2');
+    }
+    return nombre;
+
+  }
+
 }
