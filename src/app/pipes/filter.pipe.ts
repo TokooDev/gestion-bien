0d4b5743ff7da1fb895b03278574d244d1b8b1bc
@Tokodev <layesall05@gmail.com>
2020-12-23 08:05:53 +0000

    Ajout de services

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, search: string, propertyName: string): any {
-    if(value.length == 0 || search.length===0){
+    if(value.length === 0 || search.length === 0){
       return value;
     }
     const resultArray = [];
